c507e0610b91676a3babc3ba6144a4f240540b88
HMY <hemingyuna@gmail.com>
2025-07-02 11:02:24 +0800

    Squash merge dev into master

---
+++ 数据结构练习/chain.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Chain
+{
+   public:
+    Chain(int val)
+    {
+        this->val = val;
+        this->next = nullptr;
+    }
+    ~Chain()
+    {
+        delete this->next;
+    }
+    Chain* set_val(int val)
+    {
+        this->val = val;
+        return this;
+    }
+    int get_val()
+    {
+        return val;
+    }
+    Chain* add_val(int delta)
+    {
+        val += delta;
+        return this;
+    }
+    Chain* set_next(Chain* next)
+    {
+        this->next = next;
+        return next;
+    }
+
+    Chain* print()
+    {
+        Chain* next = this;
+        cout << this->val;
+        next = next->next;
+        while (next != nullptr)
+        {
+            cout << " , " << next->val;
+            next = next->next;
+        }
+        cout << endl;
+        return this;
+    }
+
+   private:
+    int val;
+    Chain* next;
+};
+
+int main()
+{
+    int n, num;
+    Chain* next = nullptr;
+    cin >> n;
+    cin >> num;
+    Chain* head = new Chain(num);
+    next = head;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        cin >> num;
+        next = next->set_next(new Chain(num));
+    }
+    head->print();
+    delete head;
+    return 0;
+}
